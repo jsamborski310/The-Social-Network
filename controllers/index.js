3af90f1580d37017271596bcd15e3fc33516a466
Juanita Samborski <juanita@breakthroughwebsite.com>
2022-03-15 18:03:37 -0700

    Setting up files.

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-
-router.user('/users', userRoutes); //double-check if it should be router.user or router.users
-
-
-
-
-module.exports = router;
